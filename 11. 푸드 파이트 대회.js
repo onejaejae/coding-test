54c3beee0e80212c9c5a223819abaa6faa5c174a
wonjjee <yoteamo7@naver.com>
2023-07-05 00:13:19 +0900

    푸드 파이터 대회

--- 11. 푸드 파이트 대회.js
+++ 11. 푸드 파이트 대회.js
@@ -4,7 +4,7 @@ function solution(food) {
     .map((food) => parseInt(food / 2))
     .reduce((acc, cur, idx) => (acc += (idx + 1).toString().repeat(cur)), "");
 
-  console.log("res", res);
+  return res.concat("0" + res.split("").reverse().join(""));
 }
 
 console.log(solution([1, 3, 4, 6]));
