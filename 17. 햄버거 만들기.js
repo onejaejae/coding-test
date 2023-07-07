3aac708167bd4c3f43e8e90827032c13ae9ef546
wonjjee <yoteamo7@naver.com>
2023-07-07 15:12:28 +0900

    햄버거 만들기

---
+++ 17. 햄버거 만들기.js
@@ -0,0 +1,24 @@
+function solution(ingredient) {
+  let answer = 0;
+  let stack = [];
+
+  ingredient.forEach((inner) => {
+    stack.push(inner);
+    const sLength = stack.length;
+
+    if (
+      stack[sLength - 4] === 1 &&
+      stack[sLength - 3] === 2 &&
+      stack[sLength - 2] === 3 &&
+      stack[sLength - 1] === 1
+    ) {
+      for (let i = 0; i < 4; i++) {
+        stack.pop();
+      }
+      answer++;
+    }
+  });
+  return answer;
+}
+
+console.log(solution([2, 1, 1, 2, 3, 1, 2, 3, 1]));
